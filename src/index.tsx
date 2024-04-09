8617a53b22c26369237679af169c2eb5ba63558d
Res0ution <maestro@hotger.com>
2024-04-09 23:08:21 +0300

    add monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import { unpkgPlugin } from "./plugins/unpkg-plugin";
+import Editor from "./components/editor";
 
 const App = () => {
   const esBuildRef = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const initializeEsbuildService = async () => {
     await esbuild.initialize({
@@ -23,6 +24,8 @@ const App = () => {
   }, []);
 
   const transformToCode = async () => {
+    iframe.current.srcdoc = html;
+
     const { outputFiles } = await esBuildRef.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -34,16 +37,43 @@ const App = () => {
       },
     });
 
-    setCode(outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (e) => {
+            try {
+              eval(e.data);
+            } catch (e) {
+              const root = document.querySelector('#root')
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + e + '</div>'
+              console.error(e)
+            }
+          }, false)
+        </script>
+      </body>
+      <script></script>
+    </html>
+  `;
 
   return (
     <div>
+      <Editor />
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={transformToCode}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="code preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
